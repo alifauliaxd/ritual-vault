7346012a8d55771fd77eab842205c7990b559a45
alifauliaxd <alifaulia933@gmail.com>
2026-01-30 18:36:27 +0700

    add web icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,26 @@
-import "@/styles/globals.css";
+import Head from 'next/head';
+import '../styles/globals.css'; // Pastikan path css ini sesuai file aslimu
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        {/* 1. Judul Tab Browser */}
+        <title>RITUAL VAULT | Secure Protocol</title>
+        
+        {/* 2. Ikon Kecil di Tab (Favicon) */}
+        <link rel="icon" href="/logo.png" type="image/png" />
+        
+        {/* 3. (Bonus) Biar kalau link dishare di WA/Discord ada gambarnya */}
+        <meta name="description" content="Access Restricted. Secure Weekly Protocol." />
+        <meta property="og:title" content="RITUAL VAULT" />
+        <meta property="og:description" content="Can you crack the code? Enter the vault." />
+        <meta property="og:image" content="/logo.png" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
